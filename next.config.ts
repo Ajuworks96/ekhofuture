405196e5b6b87150732fccd6821ae6c5cdd6f909
Ajuworks96 <arjunworks96@gmail.com>
2026-04-09 23:41:25 +0530

    Repositioned project to root and updated repository

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "i.postimg.cc" },
+      { protocol: "https", hostname: "assets.aceternity.com" },
+      { protocol: "https", hostname: "raw.githubusercontent.com" },
+    ],
+  },
 };
 
 export default nextConfig;
